7eddd84f66e16ea9c96c2d4e852417a7a931a8c9
jensen zhang <rasengun05@gmail.com>
2021-10-18 17:20:01 -0400

    projects -> class component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import ProjectCard from "./ProjectCard.jsx";
 
-function Projects() {
-    const website = { "title": "This Website", "tech": "Pure HTML, CSS, JS", "description": "This pretty simple website you're looking at right now. Making this website taught me a lot about HTML and CSS, and also about making a website responsive.", "githubLink": "https://github.com/jensenzhng/personal-website"}
-    return (
-        <div className="container">
-            <ProjectCard {...website}></ProjectCard>
-        </div>
-    )
-}
+const website = { "title": "This Website", "tech": "Pure HTML, CSS, JS", "description": "This pretty simple website you're looking at right now. Making this website taught me a lot about HTML and CSS, and also about making a website responsive.", "githubLink": "https://github.com/jensenzhng/personal-website"}
+
+class Projects extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <ProjectCard {...website}></ProjectCard>
+            </div>
+        )
+    }
+    }
 
 export default Projects;
