d5c6084f47e353383e17d383a6202eb4bcb6b47f
jensen zhang <rasengun05@gmail.com>
2021-10-19 17:32:40 -0400

    idk

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css'
 
 
@@ -6,20 +6,32 @@ function returnHome () {
     window.location.href = '/';
 }
 
+function hi () {
+    console.log('hi')
+}
+
 let Game = () => {
     const [playedGame, setPlayedGame] = useState(window.localStorage.getItem('playedGame'));
 
+    useEffect(() => {
+        let canvas = document.getElementById('game');
+        let context = canvas.getContext('2d');
+
+
+        // document.getElementById("startButton").onclick = document.getElementById("startButton").innerHTML = 'hi';
+    }, [])
     
 
     return (<div className="bigDiv">
-            <h1 class="startButton" id="startButton">hey! let's play a game.<br/>click me and see if you can score 8.</h1>
+            <h1 className="startButton" id="startButton">hey! let's play a game.<br/>click me and see if you can score 8.</h1>
+            <button id="startGame" onClick={hi}>start game</button>
             <button id="home" onClick={returnHome}>return home</button>
 
-            <div class="gameBoardContainer peekaboo" id="gameBoardContainer">
+            <div className="gameBoardContainer peekaboo" id="gameBoardContainer">
 
                 <h1 id="gameScore">0</h1>
 
-                <canvas width="400" height="400" id="game" class="gameCanvas"></canvas>
+                <canvas width="400" height="400" id="game" className="gameCanvas"></canvas>
 
             </div>
         </div>)
