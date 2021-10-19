8d3871b4df413224a53441b2371964a9e6fa39b5
jensen zhang <rasengun05@gmail.com>
2021-10-19 10:52:16 -0400

    page loading logic for game

--- src/components/Secret/index.jsx
+++ src/components/Secret/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
 
-class Secret extends React.Component {
-    render() { 
-        return <div>Secret</div>;
+function Secret() {
+    let history = useHistory();
+    const [playedGame, setPlayedGame] = useState(window.localStorage.getItem('playedGame'))
+
+    const handlePageLoad = () => {
+        history.push('/game');
     }
+
+    if (playedGame === null) {
+        handlePageLoad();
+    }
+
+    return (
+        <div>Secret</div>
+    );
+
 }
  
 export default Secret;
