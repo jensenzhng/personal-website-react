c7d0499e47dfbfb1f19b76ebcf58c9cd4a0f9718
jensen zhang <rasengun05@gmail.com>
2021-10-20 11:11:20 -0400

    added more project cards

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,6 +4,9 @@ import ProjectCard from "../ProjectCard";
 const website = { "title": "This Website", "tech": "Pure HTML, CSS, JS", "description": "This pretty simple website you're looking at right now. Making this website taught me a lot about HTML and CSS, and also about making a website responsive.", "githubLink": "https://github.com/jensenzhng/personal-website"}
 const twitterMonitor = { "title": "Twitter Monitor", "tech": "Backend NodeJS + Discord API", "description": "A Discord bot that posts tweets in a fraction of a second - while monitoring up to 100 Twitter users. Detects any possible images, videos, and users mentioned in a tweet.", "githubLink": "https://github.com/jensenzhng/twittermonitor"};
 const orbitalVisualizer = { "title": "Orbital Visualizer", "tech": "Frontend HTML, CSS and JS + Backend Firebase, JS", "description": "Built for the NASA SpaceApps Hackathon. Over 20,000 objects orbiting Earth are displayed along with information about the body and the ability to filter by debris, rocket bodies, and payloads.", "githubLink": "https://github.com/jensenzhng/cesium-visualizer", link: 'https://orbitalvisualizer.earth/'};
+const offerIDBot = { "title": "Walmart Offer ID Bot", "tech": "Backend NodeJS + Discord API", "description": "Scrapes the Offer ID and information of a Walmart product given the SKU, or the SKU from an Offer ID. Useful when running Walmart bots that have Offer ID mode.", "githubLink": "https://github.com/jensenzhng/offer-id-bot"};
+const bbMonitorScript = { "title": "Best Buy Monitor Script", "tech": "Backend NodeJS", "description": "Scrapes the availability of a product on Best Buy.", "githubLink": "https://github.com/jensenzhng/best-buy-monitor"};
+const idyll = { "title": "Idyll (In-Progress)", "tech": "Frontend HTML, CSS and JS + Backend Firebase, NodeJS", "description": "Check back soon...", "link": "https://idyll.shop/"};
 //{ "title": "", "tech": "", "description": "", "githubLink": ""};
 
 class Projects extends React.Component {
@@ -13,6 +16,9 @@ class Projects extends React.Component {
                 <ProjectCard {...website}></ProjectCard>
                 <ProjectCard {...twitterMonitor}></ProjectCard>
                 <ProjectCard {...orbitalVisualizer}></ProjectCard>
+                <ProjectCard {...offerIDBot}></ProjectCard>
+                <ProjectCard {...bbMonitorScript}></ProjectCard>
+                <ProjectCard {...idyll}></ProjectCard>
             </div>
         )
     }
