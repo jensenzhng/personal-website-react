022fc3dd217ece6d5e018458ce956ceb197ae9f3
jensen zhang <rasengun05@gmail.com>
2021-10-20 09:31:07 -0400

    remove import in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Main from './components/Main'
 import Secret from './components/Secret'
 import Game from './components/Game';
-import SnakeGame from './components/SnakeGame';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/snakegame" component={SnakeGame} />
           <Route path="/game" component={Game} />
           <Route path="/secret" component={Secret} />
           <Route path="/" component={Main} />
